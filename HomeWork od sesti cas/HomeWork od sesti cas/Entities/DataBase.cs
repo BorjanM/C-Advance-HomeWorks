28f8471738c92372f87ff9b85e1279f8752beebc
Borjan M <119819015+BorjanM@users.noreply.github.com>
2023-05-14 14:04:34 +0200

    Add files via upload

---
+++ HomeWork od sesti cas/HomeWork od sesti cas/Entities/DataBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_od_sesti_cas.Entities
+{
+    public static class Database
+    {
+        public static List<Person> personList = new List<Person>();
+        public static List<Dog> dogList = new List<Dog>();
+    }
+}
---
+++ HomeWork od sesti cas/HomeWork od sesti cas/Entities/Dog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_od_sesti_cas.Entities
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+
+        public int AgeOfDog { get; set; }
+        public string DogsColor { get; set; }
+        public string NameOfOwner { get; set; }
+
+    }
+
+}
